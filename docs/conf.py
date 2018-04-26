12bbffc5f94a59cd161a5e3955fcc90b5a86e18c
Team BrailleRap-SP  Philippe Pacotte  Stéphane Godin <37216855+BrailleRapSP@users.noreply.github.com>
2018-04-26 22:59:40 +0200

    typo

--- docs/conf.py
+++ docs/conf.py
@@ -30,9 +30,8 @@ source_parsers = {
 
 master_doc = 'index'
 project = u'BrailleRap-SP'
-copyright = '2018-{}, BrailleRap-SP Team'.format(
-    timezone.now().year
-)
+copyright = '2018-{}, BrailleRap-SP Team'
+
 version = '1.0'
 release = '1.0'
 exclude_patterns = ['_build']
